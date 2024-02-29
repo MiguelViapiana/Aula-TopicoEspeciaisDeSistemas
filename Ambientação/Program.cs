f78c572e450499ed9639863b2a821dc2ec2bb02d
Miguel Viapiana Jung <127536583+MiguelViapiana@users.noreply.github.com>
2024-02-29 15:07:26 -0300

    .

--- Ambientação/Program.cs
+++ Ambientação/Program.cs
@@ -2,12 +2,3 @@
 Console.WriteLine("Digite a sua diade: ");
 string idade = Console.ReadLine();
 Console.WriteLine("Idade é de: " + idade);
-/*
-if(idade > 18){
-    Console.WriteLine("Você é uma criança :D");
-}else if (idade > 60){
-    Console.WriteLine("Você é um adulto :)");
-}else{
-    Console.WriteLine("Você é um idoso XD");
-}
-*/
