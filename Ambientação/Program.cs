ebb58e783e4f8b88d1391c7150ce1ef5901bffeb
Miguel Viapiana Jung <127536583+MiguelViapiana@users.noreply.github.com>
2024-02-29 11:12:14 -0300

    primeira aula

---
+++ Ambientação/Program.cs
@@ -0,0 +1,12 @@
+﻿//Exercico 0: Ler a idade como um valor inteiro
+Console.WriteLine("Digite a sua diade: ");
+string idade = Console.ReadLine();
+int i = Convert.ToInt64(idade);
+Console.WriteLine("Idade é de: " + idade);
+if(i > 18){
+    Console.WriteLine("Você é uma criança :D");
+}else if (i > 60){
+    Console.WriteLine("Você é um adulto :)");
+}else{
+    Console.WriteLine("Você é um idoso XD");
+}
