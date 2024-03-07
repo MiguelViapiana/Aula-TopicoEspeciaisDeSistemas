cf4b919ec285d384889b092aaa5a42c2bc55b8ea
MiguelViapiana <127536583+MiguelViapiana@users.noreply.github.com>
2024-03-07 11:23:39 -0300

    Exercio de sort

---
+++ ListaExercicio/Exe06A/Program.cs
@@ -0,0 +1,31 @@
+﻿//Criar um vetor
+int[] vetor = new int[100];
+
+
+//Criar o objeto que vai gerar o número randomico
+Random aleatorio = new Random();
+
+//Preencher vetor com valores
+for (int i = 0; i < vetor.Length; i++){
+    vetor[i] = aleatorio.Next(100);
+}
+Boolean troca = true;
+do{
+    troca = false;
+    for(int i = 0; i < vetor.Length-1; i++){
+        if(vetor[i] > vetor[i+1]){
+            int aux = vetor[i];
+            vetor[i] = vetor[i+1];
+            vetor[i + 1] = aux;
+            troca = true;
+        }
+    }
+}while(troca == true);
+
+
+for (int i = 0; i < vetor.Length; i++){
+    Console.Write(vetor[i] + " ");
+}
+
+
+//Visualgo, toptal
--- ListaExercicio/Exe06B/obj/Debug/net8.0/Exe06B.AssemblyInfo.cs
+++ ListaExercicio/Exe06B/obj/Debug/net8.0/Exe06B.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Ambientação")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Exe06B")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de753e6e9bd03a086c9792d32c64aef683d7d7ed")]
-[assembly: System.Reflection.AssemblyProductAttribute("Ambientação")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Ambientação")]
+[assembly: System.Reflection.AssemblyProductAttribute("Exe06B")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Exe06B")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
